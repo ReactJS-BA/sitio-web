e3e846559e0d91084f5a1ac6143ea1f97e239e6c
Miguel-Andres <silva.andresmiguel@gmail.com>
2021-05-30 18:08:01 -0300

    maquetado de la seccion de SOCIAL , intale react Icons

--- src/Social/Social.tsx
+++ src/Social/Social.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Social.css';
+import {TiSocialTwitter} from "react-icons/ti"
+import {SiSlack ,SiMeetup,SiYoutube} from "react-icons/si"
+import {MdEmail} from "react-icons/md"
 
-function Social() {
+ function Social() {
   return (
-    <div className="social">
-      Social v2
-    </div>
+    <React.Fragment>
+      <div className="social">
+        <ul>
+          <li> <div className="socialIcons" > <TiSocialTwitter size={40} /></div>Twitter</li>
+          <li> <div className="socialIcons" ><SiSlack size={40}/></div> Slack</li>
+          <li> <div className="socialIcons" > <SiMeetup size={40}/> </div>Meetup</li>
+          <li> <div className="socialIcons" ><SiYoutube  size={40}/></div>Youtube</li>
+          <li> <div className="socialIcons" ><MdEmail  size={40}/> </div>Email</li>
+        </ul>
+      </div>
+   
+    </React.Fragment>
   );
 }
 
