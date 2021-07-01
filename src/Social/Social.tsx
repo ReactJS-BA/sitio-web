793d55d2ae2b17cf94d40f9823a83d97c92afd12
Miguel-Andres <silva.andresmiguel@gmail.com>
2021-07-01 00:53:17 -0300

    add arial label & role

--- src/Social/Social.tsx
+++ src/Social/Social.tsx
@@ -7,13 +7,13 @@ import {MdEmail} from "react-icons/md"
  function Social() {
   return (
  
-      <div className="social">
+      <div className="social" aria-label="redes sociales" role="img">
         <ul>
-          <li> <div className="socialIcons" > <TiSocialTwitter size={40} /></div>Twitter</li>
-          <li> <div className="socialIcons" ><SiSlack size={40}/></div> Slack</li>
-          <li> <div className="socialIcons" > <SiMeetup size={40}/> </div>Meetup</li>
-          <li> <div className="socialIcons" ><SiYoutube  size={40}/></div>Youtube</li>
-          <li> <div className="socialIcons" ><MdEmail  size={40}/> </div>Email</li>
+          <li> <div className="socialIcons" aria-label="Twitter" role="img"> <TiSocialTwitter size={40} /> </div>Twitter</li>
+          <li> <div className="socialIcons" aria-label="Slack" role="img"> <SiSlack size={40}/> </div> Slack</li>
+          <li> <div className="socialIcons" aria-label="Meetup" role="img"> <SiMeetup size={40}/> </div>Meetup</li>
+          <li> <div className="socialIcons" aria-label="Youtube" role="img"> <SiYoutube  size={40}/> </div>Youtube</li>
+          <li> <div className="socialIcons" aria-label="Email" role="img"> <MdEmail  size={40}/> </div>Email</li>
         </ul>
       </div>
    
