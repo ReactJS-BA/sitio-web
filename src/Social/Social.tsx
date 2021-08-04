86285bd1986a95502014974560d20c2b41df4bec
Rodrigo Avila <rodrigo398@gmail.com>
2021-08-04 19:48:17 +1000

    refactor: address feedback from PR

--- src/Social/Social.tsx
+++ src/Social/Social.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import './Social.css';
-import {TiSocialTwitter} from "react-icons/ti"
-import {SiSlack ,SiMeetup,SiYoutube} from "react-icons/si"
-import {MdEmail} from "react-icons/md"
+import { TiSocialTwitter } from "react-icons/ti";
+import { SiSlack, SiMeetup, SiYoutube } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
+import "./Social.css";
 
- function Social() {
+function Social() {
   return (
- 
-      <div className="social" aria-label="redes sociales" role="img">
-        <ul>
-          <li> <div className="socialIcons" aria-label="Twitter" role="img"> <TiSocialTwitter size={40} /> </div>Twitter</li>
-          <li> <div className="socialIcons" aria-label="Slack" role="img"> <SiSlack size={40}/> </div> Slack</li>
-          <li> <div className="socialIcons" aria-label="Meetup" role="img"> <SiMeetup size={40}/> </div>Meetup</li>
-          <li> <div className="socialIcons" aria-label="Youtube" role="img"> <SiYoutube  size={40}/> </div>Youtube</li>
-          <li> <div className="socialIcons" aria-label="Email" role="img"> <MdEmail  size={40}/> </div>Email</li>
-        </ul>
-      </div>
-   
-   
+    <div className="social" aria-label="redes sociales" role="group">
+      <ul>
+        <li>
+          <a
+            href="https://twitter.com/React_BA"
+            className="socialIcons"
+            aria-label="Twitter"
+            role="img"
+            target="_blank"
+          >
+            <TiSocialTwitter size={40} />
+          </a>
+          Twitter
+        </li>
+        <li>
+          <a
+            href="slack://channel?team=T0434657X&id=C045DPHNR"
+            className="socialIcons"
+            aria-label="Slack"
+            role="img"
+            target="_blank"
+          >
+            <SiSlack size={40} />
+          </a>
+          Slack
+        </li>
+        <li>
+          <a
+            href="https://www.meetup.com/React-en-Buenos-Aires"
+            className="socialIcons"
+            aria-label="Meetup"
+            role="img"
+            target="_blank"
+          >
+            <SiMeetup size={40} />
+          </a>
+          Meetup
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UC8lMWXElwhflZxWqsE6BuPQ"
+            className="socialIcons"
+            aria-label="Youtube"
+            role="img"
+            target="_blank"
+          >
+            <SiYoutube size={40} />
+          </a>
+          Youtube
+        </li>
+        <li>
+          <a
+            href="mailto:reactjs.ba@gmail.com"
+            className="socialIcons"
+            aria-label="Email"
+            role="img"
+          >
+            <MdEmail size={40} />
+          </a>
+          Email
+        </li>
+      </ul>
+    </div>
   );
- }
+}
 
 export default Social;
